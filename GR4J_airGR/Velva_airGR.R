339b49936acff80d2a0db9f3eb021446b421932e
Inlupa <52817215+Inlupa@users.noreply.github.com>
2026-06-03 12:39:01 +0300

    Апдейт расчета по вельве с комментариями

--- GR4J_airGR/Velva_airGR.R
+++ GR4J_airGR/Velva_airGR.R
@@ -1,55 +1,94 @@
+library(airGR)
+library(airGRteaching)
+
 MyGR4j = function(dates,
                   prec,
                   temp,
                   evap,
                   flow, # временные ряды переменных
-                  ZInp, # средняя высота водосбора
+                  ZInp = F, # средняя высота водосбора
                   startCalib,
                   endWarmUp, 
                   endCalib,
                   startValid,
-                  endValid # даты начала или конца соответствующих периодов
+                  endValid, # даты начала или конца соответствующих периодов
+                  transfo = F,
+                  IS = F
                   ) 
 {
-  Index_Calib_WarmUp = seq(which(dates == as.Date(startCalib)),
-                                     which(dates == as.Date(endWarmUp)))
-  Index_Calib = seq(which(dates == as.Date(startCalib)),
-                              which(dates == as.Date(endCalib)))
-  Index_Valid = seq(which(dates == as.Date(startValid)),
-                              which(dates == as.Date(endValid)))
+  # Разделение данных на периоды разогрева, калибровки и валидации
+  Index_Calib_WarmUp = seq(which(as.Date(dates) == as.Date(startCalib)),
+                                     which(as.Date(dates) == as.Date(endWarmUp)))
+  Index_Calib = seq(which(as.Date(dates) == as.Date(startCalib)),
+                              which(as.Date(dates) == as.Date(endCalib)))
+  Index_Valid = seq(which(as.Date(dates) == as.Date(startValid)),
+                              which(as.Date(dates) == as.Date(endValid)))
   
-  GR4j = airGR::CreateInputsModel(FUN_MOD = RunModel_CemaNeigeGR4J,
+  # Создание модели с обозначением входных рядов и данных о водосборе
+  if (ZInp == F) {
+     GR4j = airGR::CreateInputsModel(FUN_MOD = RunModel_CemaNeigeGR4J,
+                                     DatesR = as.POSIXct(dates),
+                                     Precip = prec,
+                                     PotEvap = evap,
+                                     TempMean = temp)
+  }
+  else {
+     GR4j = airGR::CreateInputsModel(FUN_MOD = RunModel_CemaNeigeGR4J,
                                             DatesR = as.POSIXct(dates),
                                             Precip = prec,
                                             PotEvap = evap,
                                             TempMean = temp,
                                             ZInputs = ZInp)
+  }
+  
+  
+  # Выбор настроек калибровки (тип модели, модель водосбора, периоды разогрева и непосредственно калибровки)
   RunOptions_Calib = airGR::CreateRunOptions(FUN_MOD = RunModel_CemaNeigeGR4J,
                                                        InputsModel = GR4j,
                                                        IndPeriod_WarmUp = Index_Calib_WarmUp,
                                                        IndPeriod_Run = Index_Calib)
+  
+  # Установка опций калибровки (тип модели, алгоритм калибровки)
   CalibOptions = airGR::CreateCalibOptions(FUN_MOD = RunModel_CemaNeigeGR4J,
                                                      FUN_CALIB = Calibration_Michel)
-  
-  InputsCrit = airGR::CreateInputsCrit(FUN_CRIT = ErrorCrit_NSE,
+
+  # Выбор критерия оптимизации (функции, модели водосбора, опций калибровки и ряда фактических значений)
+  if (transfo == F) {
+     InputsCrit = airGR::CreateInputsCrit(FUN_CRIT = ErrorCrit_NSE,
                                                  InputsModel = GR4j,
                                                  RunOptions = RunOptions_Calib,
                                                  Obs = flow[Index_Calib])
+  }
+  else {
+     InputsCrit = airGR::CreateInputsCrit(FUN_CRIT = ErrorCrit_NSE,
+                                          InputsModel = GR4j,
+                                          RunOptions = RunOptions_Calib,
+                                          Obs = flow[Index_Calib],
+                                          transfo = transfo)
+  }
+  
+  
+  # Выполнение калибровки
   Calibration = airGR::Calibration_Michel(InputsModel = GR4j,
                                                     RunOptions = RunOptions_Calib,
                                                     InputsCrit = InputsCrit,
                                                     CalibOptions = CalibOptions,
                                                     FUN_MOD = RunModel_CemaNeigeGR4J,
                                                     FUN_CRIT = ErrorCrit_NSE)
+  
+  # Полученные в результате калибровки оптимальные параметры
   Params = Calibration$ParamFinalR
   
+  # Модельный расчёт для периода калибровки
   Modelling_Calib = airGR::RunModel_CemaNeigeGR4J(InputsModel = GR4j,
                                                             RunOptions = RunOptions_Calib,
                                                             Param = Calibration$ParamFinalR)
+  # Комплексный график для периода калибровки
   plotCalib = plot(Modelling_Calib,
        Qobs = flow[Index_Calib])
   
-  IniStates= airGR::CreateIniStates(FUN_MOD = RunModel_CemaNeigeGR4J,
+  # Начальные условия расчёта
+  IniStates = airGR::CreateIniStates(FUN_MOD = RunModel_CemaNeigeGR4J,
                                               InputsModel = GR4j,
                                               ProdStore = Modelling_Calib$StateEnd$Store[[1]],
                                               RoutStore = Modelling_Calib$StateEnd$Store[[2]],
@@ -58,15 +97,30 @@ MyGR4j = function(dates,
                                               GCemaNeigeLayers = Modelling_Calib$StateEnd$CemaNeigeLayers[[1]],
                                               eTGCemaNeigeLayers = Modelling_Calib$StateEnd$CemaNeigeLayers[[2]])
   
-  RunOptions = airGR::CreateRunOptions(FUN_MOD = RunModel_CemaNeigeGR4J,
+  # Установка опций для периода моделирования
+  if (IS == F) {
+     RunOptions = airGR::CreateRunOptions(FUN_MOD = RunModel_CemaNeigeGR4J,
+                                          InputsModel = GR4j,
+                                          IndPeriod_Run = Index_Valid)
+  }
+  else {
+     RunOptions = airGR::CreateRunOptions(FUN_MOD = RunModel_CemaNeigeGR4J,
                                                  InputsModel = GR4j,
                                                  IndPeriod_Run = Index_Valid,
                                                  IniStates = IniStates)
+  }
+  
+  # Модельный расчёт (для валидационного периода)
   Modelling = airGR::RunModel_CemaNeigeGR4J(InputsModel = GR4j,
                                                       RunOptions = RunOptions,
                                                       Param = Calibration$ParamFinalR)
+  print('Есть модель для валидации')
+  
+  # Построение комплексного графика для периода калибровки
   plotValid = plot(Modelling,
        Qobs = flow[Index_Valid])
+  
+  # Результатом функции является список из объектов: результата калибровки, графиков, начальных условий, расчётного ряда, метрик NSE для калибровочного и валидационного периодов
   outputs = list(Calibration = Calibration,
                  #Params = Params, 
                  plotCalib = plotCalib,
@@ -83,3 +137,8 @@ MyGR4j = function(dates,
   outputs
   
 }
+
+
+
+
+
